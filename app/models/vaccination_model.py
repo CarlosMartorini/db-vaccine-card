856d414f5c7203f9d38a18892481482cd1ff4261
Carlos Martorini <c7.silveira@gmail.com>
2021-09-22 22:50:09 -0300

    feat: Created functions for blueprints routes

--- app/models/vaccination_model.py
+++ app/models/vaccination_model.py
@@ -1,11 +1,39 @@
+from datetime import date, timedelta
 from app.configs.database import db
+from dataclasses import dataclass
+from flask import request, jsonify
 
+
+@dataclass
 class Vaccination(db.Model):
+    cpf: str
+    name: str
+    first_shot_date: date
+    second_shot_date: date
+    vacine_name: str
+    health_unit_name: str
+
     __tablename__ = 'vaccine_card'
 
     cpf = db.Column(db.String(10), primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     first_shot_date = db.Column(db.Date())
-    second_shot_date = db.Column(db.Date())
+    second_shot_date = db.Column(db.Date(), first_shot_date + timedelta(days = 90))
     vacine_name = db.Column(db.String(255), nullable=False)
     health_unit_name = db.Column(db.String(255))
+
+
+    def create_vaccine_card():
+        data = request.get_json()
+
+        vaccine = Vaccination(**data)
+
+        db.session.add(vaccine)
+        db.session.commit()
+
+        return jsonify(vaccine), 201
+
+
+    def get_all_vaccines_cards():
+        vaccines_list = Vaccination.query.all()
+        return jsonify(vaccines_list), 200
--- app/routes/vaccination_blueprint.py
+++ app/routes/vaccination_blueprint.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
-from app.controllers.vaccination_controller import
+from app.models.vaccination_model import create_vaccine_card, get_all_vaccines_cards
 
 bp = Blueprint('vaccination_bp', __name__, url_prefix='/vaccination')
 
+bp.post('')(create_vaccine_card)
+bp.get('')(get_all_vaccines_cards)
